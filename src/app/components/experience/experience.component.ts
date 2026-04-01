20f83fe6107938fda87f1c2f22b8737c2dcd0545
Debojyoti bhattacherjee <bhattacharyadebojyoti2@gmail.com>
2026-04-01 23:43:09 +0530

    update

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -17,32 +17,27 @@ interface Experience {
 export class ExperienceComponent {
   experiences: Experience[] = [
     {
-      position: "Java Developer",
+      position: "Java Programing Internship",
       company: "CodeAlpha",
       companyUrl: "https://www.codealpha.tech/",
       startDate: "July 2025",
-      endDate: "Present",
+      endDate: "August 2025",
       achievements: [
-        "Designed and implemented a robust backend webhook for in-app purchases for Android, improving the reliability of payment notification handling for both subscriptions and single product purchases, resulting in a 20% decrease in transaction disputes",
-        "Built and optimized RESTful APIs to support efficient CRUD operations and high-volume data processing, significantly boosting system responsiveness and data accessibility",
-        "Integrated an AI-powered summarization and copilot API into Texpro, while crafting an intuitive UI to improve user interaction and streamline productivity.",
-        "Crafted and executed data migration scripts in SQL Server and MongoDB to ensure seamless, accurate, and efficient data transitions across systems.",
-        "Enhanced Texpro’s capabilities by introducing new features at both the UI and API layers, improving user engagement and expanding application functionality",
+        "Stock Trading Platform: Built a Java-based stock trading simulation platform featuring user registration, market data display, buy/sell operations, portfolio tracking, and dynamic price updates. Implemented using OOP principles with a modular structure and versioned using Git/GitHub.",
+
+        "Student Grade Tracker: Developed a GUI-based application using Java Swing to manage and analyze student grades. Implemented features for adding student records, displaying data in tables, and generating real-time statistics such as average, highest, and lowest grades. Designed with modular methods for data handling and UI updates, ensuring smooth user interaction and efficient performance.",
       ],
     },
     {
-      position: "Software Engineer",
-      company: "Maruti Tech Labs",
-      companyUrl: "https://www.marutitech.com",
-      startDate: "December 2021",
-      endDate: "December 2023",
+      position: "Java Programing Internship",
+      company: "CodeAlpha",
+      companyUrl: "https://www.codealpha.tech/",
+      startDate: "July 2025",
+      endDate: "August 2025",
       achievements: [
-        "Significantly contributed to the project’s success by single-handedly managing the entire project life cycle, including development, deployment, database management, and client communication",
-        "Crafted a comprehensive project solution by harnessing Angular, .NET Core, and SQL Server, resulting in a 40% increase in project efficiency",
-        "Established a streamlined workflow for Continuous Integration and Continuous Deployment (CI/CD), ensuring efficient and error-free updates to the platform.",
-        "Implemented a modular architecture, effectively separating the tightly coupled front and back-end components, enhancing maintainability and flexibility.",
-        "Upgraded the legacy .NET Framework 4.8 to the latest .NET Core 7, revolutionising the platform’s capabilities with modern technology.",
-        "Boosted performance by migrating 100% of client-side operations from the IIS server to Firebase.",
+        "Stock Trading Platform: Built a Java-based stock trading simulation platform featuring user registration, market data display, buy/sell operations, portfolio tracking, and dynamic price updates. Implemented using OOP principles with a modular structure and versioned using Git/GitHub.",
+
+        "Student Grade Tracker: Developed a GUI-based application using Java Swing to manage and analyze student grades. Implemented features for adding student records, displaying data in tables, and generating real-time statistics such as average, highest, and lowest grades. Designed with modular methods for data handling and UI updates, ensuring smooth user interaction and efficient performance.",
       ],
     },
   ];
